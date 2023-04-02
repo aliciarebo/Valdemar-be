dda5a54520adcb8e8c2df2f74f712b2789938c01
alicia <alicia.rebollo@asseco.es>
2023-04-02 09:27:05 +0200

    Creacion del proyecto

---
+++ src/main/java/com/apirest/valdemarbe/ValdemarBeApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.valdemarbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValdemarBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValdemarBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirest/valdemarbe/ValdemarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.valdemarbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValdemarBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
